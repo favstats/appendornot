e4778ca171b6d1fd9c81a0121e13416312c42fbe
favstats <fabio.votta@gmail.com>
2022-11-28 11:16:23 +0100

    update save_csv so it can handle custom columns and order

--- R/save.R
+++ R/save.R
@@ -21,7 +21,47 @@
 #' @importFrom purrr discard
 save_csv <- function(d, path, create_subfolders = T) {
   if (file.exists(path)) {
-    readr::write_csv(d, append = T, file = path)
+    appendit <- T
+
+    # write_csv(cars %>% select(dist, everything()), "cars.csv")
+    the_names <- names(readr::read_csv(path, n_max = 0, show_col_types = FALSE))
+
+    # d <- cars
+
+
+    ## check if there are names in the csv that werent in new data
+    if (!all(the_names %in% names(d))) {
+      d <- the_names %>%
+        setdiff(names(d)) %>%
+        add_columns(d, .)
+    }
+
+    ## check if there are names in the new data that werent in the csv
+    if (length(names(d)) >= length(the_names)) {
+      new_names <- names(d) %>%
+        setdiff(the_names)
+
+      old_d <- readr::read_csv(path, show_col_types = FALSE)
+
+      bind_em <- list(d, old_d)
+      d <- data.table::rbindlist(bind_em, use.names = T, fill = T)
+
+      the_names <- c(the_names, new_names)
+
+      appendit <- F
+    }
+
+    ## check if the order is the same
+    if (!any(names(d) == the_names)) {
+      d <- d %>%
+        dplyr::select(dplyr::all_of(the_names))
+    }
+
+    if (appendit) {
+      readr::write_csv(d, append = T, file = path)
+    } else if (!appendit) {
+      readr::write_csv(d, file = path)
+    }
   } else {
     if (create_subfolders) {
       dirs_to_create <- stringr::str_split(path, "\\/") %>%
@@ -36,6 +76,19 @@ save_csv <- function(d, path, create_subfolders = T) {
 
     readr::write_csv(d, file = path)
   }
+}
+
+
+
+
+add_columns <- function(df, columns) {
+  columns %>%
+    purrr::map_dfr(~ {
+      df %>%
+        mutate(
+          {{ .x }} := NA
+        )
+    })
 }
 
 
@@ -63,7 +116,7 @@ save_csv <- function(d, path, create_subfolders = T) {
 #' @importFrom purrr discard
 save_lines <- function(l, path, create_subfolders) {
   if (file.exists(path)) {
-    readr::write_lines(l, append = T, file = path)
+    readr::write_lines(l, append = T, file = path, sep = "\n")
   } else {
     if (create_subfolders) {
       dirs_to_create <- stringr::str_split(path, "\\/") %>%
@@ -79,3 +132,28 @@ save_lines <- function(l, path, create_subfolders) {
     readr::write_lines(l, file = path)
   }
 }
+
+
+### test cases
+# cars <- tibble(id = c(1,2), speed = 1:2, dist = 3:4)
+#
+# new_column_dat <- cars %>% mutate(thisisnownew = "yooo")
+# different_order <- cars %>% select(dist, everything())
+# different_order_newcolumn <- cars %>% mutate(yy = "yes") %>% select(speed, yy, everything())
+#
+# write_csv(cars, "cars.csv")
+# # readr::read_csv("cars.csv") %>% View
+#
+#
+# debugonce(save_csv)
+#
+# ## new column is added
+# save_csv(new_column_dat, "cars.csv")
+#
+# ## new column is NOT there
+# save_csv(cars, "cars.csv")
+#
+# ## new column is NOT there in different order
+# save_csv(different_order_newcolumn, "cars.csv")
+#
+# readr::read_csv("cars.csv") %>% View
